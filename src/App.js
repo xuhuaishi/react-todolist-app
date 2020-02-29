c47ac056594e8a15043898284e4e5aa99385f02d
Harry Xu (TCI) <Harry_Xu@toyota.ca>
2020-02-29 17:47:25 -0500

    add input and button for to-do-list

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return <h1>Hello World!</h1>;
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <form id="to-do-form">
+            <input type="text" placeholder="Enter Text" />
+            <button type="submit">Add</button>
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
