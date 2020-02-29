f9935eaa7b9bc782c35d6b491bd4105122ba01dc
Harry Xu (TCI) <Harry_Xu@toyota.ca>
2020-02-29 18:39:55 -0500

    add Listitems.js to show all items

---
+++ src/Listitems.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Listitems.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function ListItems(props) {
+  const items = props.items;
+  const listItems = items.map(item => {
+    return (
+      <div className="list" key="item.key">
+        <p>
+          {item.text}
+          <span>
+            <FontAwesomeIcon className="faicons" icon="trash" />
+          </span>
+        </p>
+      </div>
+    );
+  });
+  return <div>{listItems}</div>;
+}
+
+export default ListItems;
