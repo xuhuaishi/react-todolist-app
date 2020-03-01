c565c370d6eb9199d0d9abdb04f1f88ab0f6bc60
Harry Xu (TCI) <Harry_Xu@toyota.ca>
2020-02-29 22:03:39 -0500

    Updates

---
+++ precache-manifest.117cb679a0fdfb7502b14f7f98686243.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80c44cabe277c9c5dbcb6521b999cd5",
+    "url": "/react-todolist-app/index.html"
+  },
+  {
+    "revision": "e4c3c17612cb3dd6b62b",
+    "url": "/react-todolist-app/static/css/main.58eead37.chunk.css"
+  },
+  {
+    "revision": "e4ce357793ff55ee2c29",
+    "url": "/react-todolist-app/static/js/2.ca74b7bb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-todolist-app/static/js/2.ca74b7bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c3c17612cb3dd6b62b",
+    "url": "/react-todolist-app/static/js/main.c0aeb61b.chunk.js"
+  },
+  {
+    "revision": "58cd2e704f52d67c089a",
+    "url": "/react-todolist-app/static/js/runtime-main.b16d5e42.js"
+  }
+]);
